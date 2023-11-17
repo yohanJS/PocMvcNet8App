687102ad76662c5666c45606e87a7c2cd4ba25d5
Yohan Garcia <yoanvaldes03@gmail.com>
2023-11-16 22:26:07 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PocMvcNet8App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
