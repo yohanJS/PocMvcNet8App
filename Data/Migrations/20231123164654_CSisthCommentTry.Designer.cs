6f63e7c51b7da6c26bb816a6f551cab9673e8496
Yohan Garcia <yoanvaldes03@gmail.com>
2023-11-23 13:24:09 -0500

    - finally made it work - you can now add comments to a blog post - the comment will belong to the user that is logged in

--- Data/Migrations/20231123164654_CSisthCommentTry.Designer.cs
+++ Data/Migrations/20231123164654_CSisthCommentTry.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PocMvcNet8App.Data;
 
@@ -11,9 +12,11 @@ using PocMvcNet8App.Data;
 namespace PocMvcNet8App.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231123164654_CSisthCommentTry")]
+    partial class CSisthCommentTry
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -251,6 +254,9 @@ namespace PocMvcNet8App.Data.Migrations
                     b.Property<int?>("BlogModelId")
                         .HasColumnType("int");
 
+                    b.Property<string>("Comment")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<string>("Content")
                         .HasColumnType("nvarchar(max)");
 
@@ -264,6 +270,9 @@ namespace PocMvcNet8App.Data.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Title")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("TitleComment")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("UserId")
@@ -285,9 +294,6 @@ namespace PocMvcNet8App.Data.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<string>("Author")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<int?>("BlogModelId")
                         .HasColumnType("int");
